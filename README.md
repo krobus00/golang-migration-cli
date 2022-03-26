90cde68be8aa8c3101e707b6d8b91049ffcc5c58
krobus00 <michaelglaxy@gmail.com>
2022-03-26 21:08:59 +0700

    [ADD] database support

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 # command
 1. migration create <file_name>
 2. migration up
-3. migration down
+3. migration up-to <version>
+4. migration down
+5. migration down-to <version>
+6. migration redo
+7. migration status
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -11,15 +12,17 @@ import (
 	"github.com/pressly/goose"
 )
 
+var (
+	flags = flag.NewFlagSet("goose", flag.ExitOnError)
+)
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	var (
-		flags = flag.NewFlagSet("goose", flag.ExitOnError)
-
+		dir      = flags.String("dir", fmt.Sprintf("./%s", os.Getenv("MIGRATION_PATH")), "directory with migration files")
 		dbstring = flags.String("dbstring",
 			fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 				os.Getenv("DATABASE_USERNAME"),
@@ -30,40 +33,98 @@ func main() {
 			),
 			"connection string",
 		)
-		dir = flags.String("dir", fmt.Sprintf("./%s", os.Getenv("MIGRATION_PATH")), "directory with migration files")
 	)
-
+	flags.Usage = usage
 	flags.Parse(os.Args[1:])
+
 	args := flags.Args()
+
+	if len(args) > 1 && args[0] == "create" {
+		arguments := []string{}
+		arguments = append(arguments, args[1:]...)
+		arguments = append(arguments, "sql")
+		if err := goose.Run("create", nil, *dir, arguments...); err != nil {
+			log.Fatalf("goose run: %v", err)
+		}
+		return
+	}
 
 	if len(args) < 1 {
 		flags.Usage()
 		return
 	}
 
-	command := args[0]
-
-	db, err := goose.OpenDBWithDriver("mysql", *dbstring)
-	if err != nil {
-		log.Fatalf("goose: failed to open DB: %v\n", err)
+	if args[0] == "-h" || args[0] == "--help" {
+		flags.Usage()
+		return
 	}
 
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Fatalf("goose: failed to close DB: %v\n", err)
+	driver, command := os.Getenv("DATABASE_DIALECT"), args[0]
+
+	switch driver {
+	case "postgres", "mysql", "sqlite3", "redshift":
+		if err := goose.SetDialect(driver); err != nil {
+			log.Fatal(err)
 		}
-	}()
+	default:
+		log.Fatalf("%q driver not supported\n", driver)
+	}
+
+	switch *dbstring {
+	case "":
+		log.Fatalf("-dbstring=%q not supported\n", *dbstring)
+	default:
+	}
+
+	if driver == "redshift" {
+		driver = "postgres"
+	}
+
+	db, err := sql.Open(driver, *dbstring)
+	if err != nil {
+		log.Fatalf("-dbstring=%q: %v\n", *dbstring, err)
+	}
 
 	arguments := []string{}
-	if len(args) > 1 {
-		arguments = append(arguments, args[1:]...)
-	}
-
-	if command == "create" {
-		arguments = append(arguments, "sql")
+	if len(args) > 3 {
+		arguments = append(arguments, args[3:]...)
 	}
 
 	if err := goose.Run(command, db, *dir, arguments...); err != nil {
-		log.Fatalf("goose %v: %v", command, err)
+		log.Fatalf("goose run: %v", err)
 	}
 }
+
+func usage() {
+	log.Print(usagePrefix)
+	flags.PrintDefaults()
+	log.Print(usageCommands)
+}
+
+var (
+	usagePrefix = `Usage: goose [OPTIONS] DRIVER DBSTRING COMMAND
+Drivers:
+    postgres
+    mysql
+    sqlite3
+    redshift
+Examples:
+    goose status
+    goose create init
+    goose create add_some_column
+    goose create fetch_user_data
+    goose up
+Options:
+`
+
+	usageCommands = `
+Commands:
+    up                   Migrate the DB to the most recent version available
+    up-to VERSION        Migrate the DB to a specific VERSION
+    down                 Roll back the version by 1
+    down-to VERSION      Roll back to a specific VERSION
+    redo                 Re-run the latest migration
+    status               Dump the migration status for the current DB
+    version              Print the current version of the database
+`
+)
